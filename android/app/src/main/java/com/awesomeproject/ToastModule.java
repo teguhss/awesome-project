a42d80028bbf59499f2529b6b81849a5872d004e
Teguh Suryo Santoso <teguh.s.santoso@gdplabs.id>
2019-01-15 12:21:46 +0700

    Initial Commit and Native Module Examples

---
+++ android/app/src/main/java/com/awesomeproject/ToastModule.java
@@ -0,0 +1,70 @@
+// ToastModule.java
+
+package com.awesomeproject;
+
+import android.widget.Toast;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  private ReactContext reactContext;
+
+  public ToastModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "ToastExample";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+
+  @ReactMethod
+  public void testCallback(Callback callback) {
+    callback.invoke("This is message from native module callback");
+  }
+
+  @ReactMethod
+  public void testPromise(Promise promise) {
+    promise.resolve("This is message from native module promise");
+  }
+
+  private void sendEvent(ReactContext reactContext, String eventName, WritableMap params) {
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+  }
+
+  @ReactMethod
+  public void testEvent(String eventName) {
+    WritableMap map = Arguments.createMap();
+    map.putString("message", "This is message from native module events");
+    sendEvent(this.reactContext, eventName, map);
+  }
+}
